0f8d167ffcdde91b184a619c71c297feaf890676
losha <83345377+0xlosha@users.noreply.github.com>
2022-03-29 16:42:06 +0200

    Update Deployer.sol

--- Deployer.sol
+++ Deployer.sol
@@ -3,7 +3,30 @@ pragma solidity ^0.8.9;
 
 import "./underground.sol";
 
-contract Deployer {
+contract Factory {
+    event Deployed(address addr, uint salt);
+
+    function getBytecode(
+        string memory _name,
+        string memory _symbol,
+        string memory _uri,
+        address _recipient
+    ) public pure returns (bytes memory) {
+        bytes memory bytecode = type(underground).creationCode;
+        return abi.encodePacked(bytecode, abi.encode(_name, _symbol, _uri, _recipient));
+    }
+
+    function getAddress(bytes memory bytecode, uint _salt)
+        public
+        view
+        returns (address)
+    {
+        bytes32 hash = keccak256(
+            abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(bytecode))
+        );
+        return address(uint160(uint(hash)));
+    }
+
     function deploy(
         string memory _name,
         string memory _symbol,
